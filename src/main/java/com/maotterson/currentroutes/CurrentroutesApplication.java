4599d2e3dc691611876a9e7c66181253a0145bf3
maotterson <otterson.2@gmail.com>
2022-10-28 22:30:34 -0400

    Initial commit.

---
+++ src/main/java/com/maotterson/currentroutes/CurrentroutesApplication.java
@@ -0,0 +1,13 @@
+package com.maotterson.currentroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentroutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrentroutesApplication.class, args);
+    }
+
+}
